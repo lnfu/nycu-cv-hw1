52d9f2e7ccffd949219e1e684279e6107957cbe1
En-Fu Liao <efliao@cs.nctu.edu.tw>
2025-03-05 17:11:44 +0000

    feat: try resnet101

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nycu-cv-hw1',
+    packages=find_packages(exclude=[]),
+    version='0.0.1',
+    license="Proprietary",
+    description='NYCU CV 2025 Spring - Homework 1',
+    author='Enfu Liao',
+    author_email='enfu.liao.cs10@nycu.edu.tw',
+    long_description_content_type='text/markdown',
+    url='https://github.com/lnfu/nycu-cv-hw1',
+    keywords=[
+        'ResNet',
+    ],
+    install_requires=[
+        'torch==2.6.0',
+    ],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: Other/Proprietary License',
+        'Programming Language :: Python :: 3.12',
+    ],
+)
