b1e91784c5cad7999ea575d015f973a80792d469
En-Fu Liao <efliao@cs.nctu.edu.tw>
2025-03-11 11:26:37 +0000

    feat: test

--- nycu_cv_hw1/test.py
+++ nycu_cv_hw1/test.py
@@ -30,7 +30,7 @@ class TestDataset(torch.utils.data.Dataset):
         # TODO 如果不是 pathlib.Path raise Exception
 
         self.image_file_paths = sorted(
-            pathlib.Path(image_dir_path).glob('*.jpg')
+            pathlib.Path(image_dir_path).glob("*.jpg")
         )  # 讀取所有圖片
         self.transform = transform
 
@@ -46,17 +46,40 @@ class TestDataset(torch.utils.data.Dataset):
 
 
 def main():
+
+    device = torch.torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    logging.info(f"device: {device}")
+
+    num_classes = 100  # TODO
+    backbone = torchvision.models.resnet101(
+        weights=torchvision.models.ResNet101_Weights.DEFAULT, progress=True
+    )
+    model = torch.load("models/20250311_093933.pt", weights_only=False)
+    model.eval()
+
     transform = torchvision.models.ResNet101_Weights.DEFAULT.transforms()
-    test_dataset = TestDataset(image_dir_path=DATA_DIR_PATH / "test", transform=transform)
+    test_dataset = TestDataset(
+        image_dir_path=DATA_DIR_PATH / "test", transform=transform
+    )
     test_loader = torch.utils.data.DataLoader(
         test_dataset, batch_size=config.batch_size, shuffle=False, num_workers=1
     )
-    
-    for image, image_name in test_loader:
-        print(type(image))
-        print(image_name)
-    
-    print(len(test_dataset))
+
+
+    print("image_name,pred_label")
+
+    for inputs, image_names in test_loader:  # TODO inputs or images (naming)
+        inputs = inputs.to(device)  # TODO type hint
+
+        outputs = model(inputs)
+
+        # TODO 印出來, 一個 row 一筆資料 (output, image_name)
+        # image_name,pred_label
+
+        for output, image_name in zip(outputs, image_names):
+            _, index = torch.max(output, dim=0)
+            print(image_name, end=",")
+            print(index.item(), end="\n")
 
 
 if __name__ == "__main__":
